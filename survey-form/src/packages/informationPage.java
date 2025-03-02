8e5033efe151db960c37990c3ff2b3594e9e0f1a
Mahakalaie <Mahakalaie1@gmail.com>
2025-03-02 22:24:37 +0330

    Update informationPage.java
    fix mistake in informationPage.java

--- survey-form/src/packages/informationPage.java
+++ survey-form/src/packages/informationPage.java
@@ -14,8 +14,6 @@ public class informationPage {
 
         Font myFont = new Font("Arial", Font.PLAIN, 10);
 
-        Font myFont = new Font("Arial", Font.PLAIN, 10);
-
         // Name Label and Text Field
         JLabel nameLabel = new JLabel("Name");
         nameLabel.setBounds(10, 10, 100, 20);
